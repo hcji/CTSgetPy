aacacb0e37fe65a1a3c521380bd75cdba4345b4b
hcji <ji.hongchao@foxmail.com>
2019-04-08 16:57:46 +0800

    tests

--- test/test.py
+++ test/test.py
@@ -1,14 +1,14 @@
 ### Import package
-	from CTSgetPy import CTSgetPy
+from CTSgetPy import CTSgetPy
 
 ### View possible translation options between > 200 databases.
-	CTSgetPy.CTS_options()
+CTSgetPy.CTS_options()
 	
 ### Example usage
-	# single target, single identifier
-	CTSgetPy.CTSget('KEGG', 'PubChem SID', 'C00001', top_only=True) 
-	CTSgetPy.CTSget('KEGG', 'PubChem SID', 'C00001', top_only=False) 
-	# multi targets, single identifier
-	CTSgetPy.CTSget('KEGG', ['PubChem CID', 'PubChem SID'], 'C00001')
-	# multi target, multi identifiers	
-	CTSgetPy.CTSget('KEGG', ['PubChem CID', 'PubChem SID'], ['C00001', 'C00002']) 
+# single target, single identifier
+CTSgetPy.CTSget('KEGG', 'PubChem SID', 'C00001', top_only=True) 
+CTSgetPy.CTSget('KEGG', 'PubChem SID', 'C00001', top_only=False) 
+# multi targets, single identifier
+CTSgetPy.CTSget('KEGG', ['PubChem CID', 'PubChem SID'], 'C00001')
+# multi target, multi identifiers	
+CTSgetPy.CTSget('KEGG', ['PubChem CID', 'PubChem SID'], ['C00001', 'C00002']) 
